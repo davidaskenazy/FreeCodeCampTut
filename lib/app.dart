bba3570529840c8666380c0613e84145b8458a0e
davidaskenazy <davidask.da@gmail.com>
2020-10-23 18:10:06 -0300

    customs style's and assets(fonts) imports

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:FreeCodeCampTut/style.dart';
 import 'package:flutter/material.dart';
 import 'screens/location_detail/location_detail.dart';
 
@@ -5,7 +6,16 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LocationDetail(),
-    );
+        home: LocationDetail(),
+        theme: ThemeData(
+            appBarTheme: AppBarTheme(
+              //property title remplaced for headline6 because is a deprecated...
+              //check this out
+              textTheme: TextTheme(headline6: AppBarTextStyle),
+            ),
+            textTheme: TextTheme(
+              headline6: TitleTextStyle,
+              bodyText2: Body1TextStyle,
+            )));
   }
 }
